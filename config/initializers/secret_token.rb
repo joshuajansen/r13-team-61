33b48c69e4ddd7dedd7a1d8516dd07a88a1bf9a7
Joshua Jansen <joshua@itflows.nl>
2013-10-19 21:11:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flipsum::Application.config.secret_key_base = '40ecc0ca083ef99606b6a83b973cceae89dedb597853cbef55868e518be8a1f4b9122234603fa68b839d5ea8f9e16fa289238b99f4037021ca1916872dac58ff'
