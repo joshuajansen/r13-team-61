687365c0e766f51a6bc93efeb2680b9c15662ef2
Joshua Jansen <joshua@itflows.nl>
2013-10-19 22:38:23 +0200

    Set tenant by url instead of subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_organisation_tenant
+  set_current_tenant_through_filter
+
+  def current_organisation
+    return @current_organisation if defined?(@current_organisation)
+    @current_organisation = Organisation.where(url: request.host).first
+  end
+
+  def set_organisation_tenant
+    set_current_tenant(current_organisation)
+  end
+
 end
