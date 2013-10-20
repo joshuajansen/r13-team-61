fa75e19c8d978ac529c952db7fe88277a547e2f8
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-10-20 20:47:17 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019210233) do
+ActiveRecord::Schema.define(version: 20131020184657) do
 
   create_table "issues", force: true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20131019210233) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "sort_order"
   end
 
 end
