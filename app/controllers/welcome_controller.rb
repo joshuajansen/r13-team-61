90dc8c82abcd6d82b99441b601ad158b03ec2553
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-10-21 01:36:55 +0200

    Show 8 latest issues on frontpage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @organisation = Organisation.new
-    @latest_issues = Issue.limit(10).newest
+    @latest_issues = Issue.limit(8).newest
     render layout: 'frontpage'
   end
 end
